5056d28888036cd67dfb6a4a61baee94497a9ecd
liuyuyang <2281487673@qq.com>
2023-08-08 10:01:58 +0800

    为方法添加了传参异常的检测

--- src/calculateAverage/index.js
+++ src/calculateAverage/index.js
@@ -1,4 +1,7 @@
 export function calculateAverage(arr) {
+    if (!(arr instanceof Array)) {
+        throw new Error(`${arr} is not a Array`);
+    }
     let num = 0;
     arr.map((i)=>{
         num+=i;
--- src/designateRepeat/index.js
+++ src/designateRepeat/index.js
@@ -1,5 +1,10 @@
-export function designateRepeat(str,target) {
+/* 计算某一值在字符串中出现的次数 */
+export function designateRepeat (str, target) {
+    if (typeof str !== "string") {
+      throw new Error(`${str} is not a string`);
+    }
     const regex = new RegExp(target, 'g');
     const matches = str.match(regex);
     return matches ? matches.length : 0;
-}
+  }
+  
